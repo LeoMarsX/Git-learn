2136b0f7cf619719b339674c9efc22abea9e2503
Leo <Leo_Mars1999@outlook.com>
2020-06-26 00:26:54 +0800

    德州扑克修改2

--- Algorithm/src/TexaHoldemTest.java
+++ Algorithm/src/TexaHoldemTest.java
@@ -2,16 +2,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 public class TexaHoldemTest {
     String white1 = "2D3H5C9SKH";
-    String black1 = "2H3D5S9CKD"; // Tie
+    String black1 = "2H3D5S9CKD";
 
     String white2 = "2C3H4S5C6H";
-    String black2 = "2H3H5H9HkH"; // Black Win
+    String black2 = "2H3H5H9HkH";
 
     String white3 = "2S8SASQS3S";
-    String black3 = "2H4S4C2D4H"; // White Win
+    String black3 = "2H4S4C2D4H";
 
     String white4 = "2C3H4S8CAH";
-    String black4 = "2H3D5S9CKD"; // White Wins---2
+    String black4 = "2H3D5S9CKD";
 
     @Test
     public void main() {
